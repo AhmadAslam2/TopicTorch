2d604710088c2fef4979a6cc1579cbb1d6403e0b
Ahmad Aslam <ahmadaslam1999@gmail.com>
2024-01-15 14:16:35 +0500

    update placeholder config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAoBpChYKv8o-GygQEaLeBrPnwWXpu8rcM",
-  authDomain: "topictorch-57ec4.firebaseapp.com",
-  projectId: "topictorch-57ec4",
-  storageBucket: "topictorch-57ec4.appspot.com",
-  messagingSenderId: "377704649113",
-  appId: "1:377704649113:web:1a9720493ab527e0ca8308",
-  measurementId: "G-ZVTLJBJWQM"
+  apiKey: "XXXX",
+  authDomain: "XXXXX",
+  projectId: "XXXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXX",
+  appId: "XXXX",
+  measurementId: "XXXX"
 };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
